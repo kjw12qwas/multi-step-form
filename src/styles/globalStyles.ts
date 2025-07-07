a1272847d1abfb1804e98a7db7840067b6a5ba12
kjw12qwas <kjw12qwas@daum.net>
2025-07-08 00:18:39 +0900

    chore(project-setup): 초기 폴더 구조 및 설정 파일 생성

---
+++ src/styles/globalStyles.ts
@@ -0,0 +1,16 @@
+
+import { css } from '@emotion/react';
+
+export const globalStyles = css`
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+
+  body {
+    font-family: sans-serif;
+    background-color: #f9f9f9;
+    color: #333;
+  }
+`;
