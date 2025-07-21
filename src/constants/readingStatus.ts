680fd2a39f476b55b6763756bb8cbc92b11ef3e6
kjw12qwas <kjw12qwas@daum.net>
2025-07-21 21:35:08 +0900

    docs(constants): 독서 상태 enum 주석 추가

--- src/constants/readingStatus.ts
+++ src/constants/readingStatus.ts
@@ -1,8 +1,8 @@
 export enum ReadingStatus {
-  WANT_TO_READ = "wantToRead",
-  READING = "reading", 
-  READ = "read",
-  ON_HOLD = "onHold"
+  WANT_TO_READ = "wantToRead", // 읽고 싶은 책
+  READING = "reading",         // 읽는 중
+  READ = "read",               // 읽음
+  ON_HOLD = "onHold"           // 보류중
 }
 
 export const READING_STATUS_LABELS = {
